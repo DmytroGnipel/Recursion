b6ae9d97712e589af08cf42e069090250cb07e0a
Dmytro Hnipel <dmitrognipel@gmail.com>
2024-01-09 13:44:34 +0200

    Function fibs successfully forks

---
+++ fibs.js
@@ -0,0 +1,26 @@
+/*Using iteration, write a function fibs
+which takes a number and returns an array
+containing that many numbers from the Fibonacci sequence. 
+Using an example input of 8, this function should
+return the array [0, 1, 1, 2, 3, 5, 8, 13].
+*/
+
+const fibs = number => {
+    if (number == 1) return [0]
+    if (number == 2) return [0,1]
+    const result = [0,1]    
+    let a = 0
+    let b = 1
+    let sum
+    let i = 3
+    while (i <= number) {
+        sum = a + b
+        result.push(sum)
+        a = b
+        b = sum
+        i++
+    }
+    return result
+}
+
+console.log(fibs(8)) // [0, 1, 1,  2, 3, 5, 8, 13]
