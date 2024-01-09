47282140c3138f03bf9d786dd403cb75e496a57d
Dmytro Hnipel <dmitrognipel@gmail.com>
2024-01-09 20:58:35 +0200

    Add  assignment in form of comments

--- mergeSort.js
+++ mergeSort.js
@@ -1,3 +1,11 @@
+/*Build a function mergeSort
+that takes in an array and returns a sorted array,
+using a recursive merge sort methodology. 
+An input of [3, 2, 1, 13, 8, 5, 0, 1] 
+should return [0, 1, 1, 2, 3, 5, 8, 13], 
+and an input of [105, 79, 100, 110] 
+should return [79, 100, 105, 110].*/
+
 const mergeSort = array => {
     if (array.length < 2) return array
     const middle = Math.floor(array.length / 2)
